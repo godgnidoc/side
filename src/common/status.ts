c9dd41f4787ee0a354af5abd47e40b7b5ec37767
godgnidoc <godgnidoc@gmail.com>
2023-04-27 00:43:45 +0800

    增强钩子脚本的效率和安全性,美化status打印效果

--- src/common/status.ts
+++ src/common/status.ts
@@ -1,9 +1,16 @@
-import { Feature } from "@godgnidoc/decli";
-import { getFinalTarget, projectName } from "../environment";
+import { Feature, ShortOpt } from "@godgnidoc/decli"
+import { getFinalTarget, projectName, projectPath } from "../environment"
+import { ChildProcess, ChildProcessWithoutNullStreams, spawn } from "child_process"
 
-export const statusFeature = new class extends Feature {
+class StatusFeature extends Feature {
     brief = 'Show project status'
     description = 'Show project status'
+
+    @ShortOpt('-s')
+    short = false
+
+    @ShortOpt('-n')
+    noGit = false
 
     async entry(): Promise<number> {
         console.debug('status')
@@ -21,7 +28,44 @@ export const statusFeature = new class extends Feature {
         let second = target ? target.target : 'no target'
         let third = target ? target.stage : 'no stage'
 
-        console.log(`[${prefix1}${first}${suffix} : ${prefix2}${second}${suffix} : ${prefix2}${third}${suffix}]`)
-        return 0
+        let ch: ChildProcess | ChildProcessWithoutNullStreams
+        if (this.short) {
+            console.log(`${prefix1}${first}${suffix} : ${prefix2}${second}${suffix} : ${prefix2}${third}${suffix}`)
+            if (!this.noGit) spawn('git', ['status', '-s'], { stdio: 'inherit' })
+        } else {
+            if (target) {
+                process.stdout.write(`Project ${prefix1}${first}${suffix} with target ${prefix2}${second}${suffix} and stage ${prefix2}${third}${suffix}`)
+            } else {
+                process.stdout.write(`Project ${prefix1}${first}${suffix} ${prefix2}without target${suffix}`)
+            }
+            if (this.noGit) {
+                process.stdout.write('\n')
+            } else {
+                const color = process.stdout.isTTY
+                    ? '-c color.status=always'
+                    : ''
+                ch = spawn(`git ${color} status`, {
+                    stdio: ['inherit', 'pipe', 'inherit'], shell: '/bin/bash', cwd: projectPath
+                })
+                ch.stdout.once('data', (data) => {
+                    let lines: string = data.toString()
+                    if (lines.startsWith('On branch')) {
+                        lines = lines[0].toLowerCase() + lines.slice(1)
+                        process.stdout.write(', ' + lines)
+                    } else {
+                        process.stdout.write('\n')
+                    }
+                    ch.stdout.pipe(process.stdout)
+                })
+            }
+        }
+
+        if (!ch) return 0
+        return new Promise<number>((resolve) => {
+            ch.on('exit', (code) => {
+                resolve(code || 0)
+            })
+        })
     }
 }
+export const statusFeature = new StatusFeature
