9bd8c253e41caca5273f82c9148674c2d8375dc0
godgnidoc <godgnidoc@gmail.com>
2023-05-31 10:12:07 +0800

    升级dist publish指令，允许传入多个包路径一起上传

--- src/side/dist/package/publish.ts
+++ src/side/dist/package/publish.ts
@@ -9,9 +9,9 @@ import * as pstream from 'progress-stream'
 import * as progress from 'progress'
 
 class DistPublishFeature extends Feature {
-    args = '<path/to/package>'
-    brief = 'Publish a packed package'
-    description = 'Publish a packed package\n'
+    args = '<path/to/package>...'
+    brief = 'Publish packed package(s)'
+    description = 'Publish packed package(s)\n'
         + '  path/to/package: path to the package file'
 
     @Brief('Allow overwrite existing package')
@@ -27,37 +27,39 @@ class DistPublishFeature extends Feature {
         else return Compgen('file')
     }
 
-    async entry(path: string): Promise<number> {
-        if (!path) {
+    async entry(...paths: string[]): Promise<number> {
+        if (paths.length === 0) {
             console.error('Package file required')
             return 1
         }
 
-        try {
-            const raw = await promisify(exec)('tar -xOf ' + path + ' meta/manifest')
-            const manifest = JSON.parse(raw.stdout)
-            if (!validateSync<PackageManifest>(manifest, 'PackageManifest'))
-                throw new Error(getLastValidateErrorText('PackageManifest'))
-            if (manifest instanceof Error) throw manifest
+        for (const path of paths) {
+            try {
+                const raw = await promisify(exec)('tar -xOf ' + path + ' meta/manifest')
+                const manifest = JSON.parse(raw.stdout)
+                if (!validateSync<PackageManifest>(manifest, 'PackageManifest'))
+                    throw new Error(getLastValidateErrorText('PackageManifest'))
+                if (manifest instanceof Error) throw manifest
 
-            const { size } = await stat(path)
-            const rs = createReadStream(path)
-            const str = pstream({ length: size, time: 100 })
-            const bar = new progress(`publish:    uploading ${manifest.packageId} [:bar] :percent :etas`, {
-                complete: '>',
-                incomplete: ' ',
-                width: 20,
-                total: size,
-            })
-            str.on('progress', p => bar.tick(p.delta))
-            const res = await api.package.publish(manifest, rs.pipe(str), this.allowOverwrite, this.allowDowngrade)
-            if (res.status !== 0) {
-                console.error('Publish failed: %s', res.message)
+                const { size } = await stat(path)
+                const rs = createReadStream(path)
+                const str = pstream({ length: size, time: 100 })
+                const bar = new progress(`publish:    uploading ${manifest.packageId} [:bar] :percent :etas`, {
+                    complete: '>',
+                    incomplete: ' ',
+                    width: 20,
+                    total: size,
+                })
+                str.on('progress', p => bar.tick(p.delta))
+                const res = await api.package.publish(manifest, rs.pipe(str), this.allowOverwrite, this.allowDowngrade)
+                if (res.status !== 0) {
+                    console.error('Publish failed: %s', res.message)
+                    return 1
+                }
+            } catch (e) {
+                console.error('Failed to read package manifest: %s', e.message)
                 return 1
             }
-        } catch (e) {
-            console.error('Failed to read package manifest: %s', e.message)
-            return 1
         }
 
         return 0
